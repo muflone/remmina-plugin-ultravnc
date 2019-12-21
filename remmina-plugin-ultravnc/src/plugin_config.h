8779ed07400cb59b3f5630bc5f5e4c31e46a325b
Muflone <webreg@vbsimple.net>
2019-12-21 20:19:11 +0100

    Updated for Builder 1.3.0.0

--- remmina-plugin-ultravnc/src/plugin_config.h
+++ remmina-plugin-ultravnc/src/plugin_config.h
@@ -19,11 +19,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __PLUGIN_CONFIG_H
-  #define __PLUGIN_CONFIG_H
+#pragma once
 
-  #define PLUGIN_NAME        "ULTRAVNC"
-  #define PLUGIN_DESCRIPTION "ULTRAVNC - Open a VNC connection with UltraVNC viewer"
-  #define PLUGIN_VERSION     "1.2.3.0"
-  #define PLUGIN_APPICON     "remmina-ultravnc"
-#endif
+#define PLUGIN_NAME        "ULTRAVNC"
+#define PLUGIN_DESCRIPTION "ULTRAVNC - Open a VNC connection with UltraVNC viewer"
+#define PLUGIN_VERSION     "1.3.0.0"
+#define PLUGIN_APPICON     "remmina-ultravnc"
